f0c3f7d2a6d4f64bf7cb6233c7d017095a4fc121
magnet <86327263+carolesdaughter@users.noreply.github.com>
2023-02-11 18:03:23 -0800

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,55 @@
+local Rayfield = game:HttpGet("https://raw.githubusercontent.com/carolesdaughter/Rayfield/main/Source.lua", true)
+
+local Library = loadstring(Rayfield)()
+
+local Window = Library:CreateWindow({
+    Name = "Magnet's Test UI",
+    LoadingTitle = "Magnet was here ^_^",
+    ConfigurationSaving = {
+      Enabled = false,
+      FolderName = nil,
+      FileName = "MagnetWasHere"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "", 
+      RememberJoins = true
+   },
+   KeySystem = false
+})
+
+local Main = Window:CreateTab("Main")
+local Button = Main:CreateButton({
+   Name = "Test",
+   Callback = function()
+       print("Hi")
+   end
+})
+local Toggle = Main:CreateToggle({
+   Name = "Test",
+   CurrentValue = false,
+   Flag = "TestToggle",
+   Callback = function(Value)
+        print(Value)
+   end
+})
+local Slider = Main:CreateSlider({
+   Name = "Test",
+   Range = {0, 50},
+   Increment = 0.1,
+   Suffix = "Test",
+   CurrentValue = 10,
+   Flag = "TestSlider",
+   Callback = function(Value)
+        print(Value)
+   end,
+})
+local Dropdown = Main:CreateDropdown({
+   Name = "Test",
+   Options = {"Test","Test"},
+   CurrentOption = "Test",
+   Flag = "TestDropdown",
+   Callback = function(Option)
+        print(Option)
+   end,
+})
